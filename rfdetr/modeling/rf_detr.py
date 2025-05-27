0b3c0280612cd21c8a92e8b2b6eed44f40c4c18e
Suchi Sharma <101873465+SuchiVats@users.noreply.github.com>
2025-05-27 13:40:31 +0530

    Delete rfdetr directory

--- rfdetr/modeling/rf_detr.py
+++
@@ -1,6 +0,0 @@
-from rfdetr.models.rf_detr import build_rf_detr_model as build_rf_detr
-from rfdetr.util.misc import NestedTensor  # assuming you use it
-
-def build_model(args):
-    model, criterion, postprocessors = build_rf_detr(args)
-    return model, criterion, postprocessors
--- rfdetr/models/rf_detr.py
+++
@@ -1,5 +0,0 @@
-from rfdetr.models.lwdetr import build_lw_detr as build_rf_detr
-  # Make sure lwdetr.py exists
-
-def build_rf_detr_model(args):
-    return build_rf_detr(args)
